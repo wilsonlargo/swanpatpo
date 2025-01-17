66bf3a92bad789c9485e78aaae1e9774e7b6e16f
wilsonlargo <136187461+wilsonlargo@users.noreply.github.com>
2025-01-17 17:17:19 -0500

    update

--- CONFIG/manager_edu.js
+++ CONFIG/manager_edu.js
@@ -6,15 +6,27 @@ function openIni(email){
     byE("btnRegistrarse").hidden = true
     console.clear()
 }
+
+let global_escuela={}
+
 function open_escuela(){
     const escuela=GLOBAL.state.escuela
+    let vigencias=[]
     for(id in escuela){
         if (escuela[id].id=="PROYECTO"){
-            console.log(escuela[id].nombre)
-                byE("Nombre_proyecto").textContent=escuela[id].nombre
-        }
-    }
+            global_escuela["proyecto"]=escuela[id].nombre
+            byE("Nombre_proyecto").textContent=escuela[id].nombre
+        } else if(escuela[id].id.includes("V-")==true){
+            vigencias.push(escuela[id])
+            //console.log(escuela[id])
 
+        } else if(escuela[id].id=="TABLAS"){
+            global_escuela["tablas"]=escuela[id]
+        }
+
+    }
+    global_escuela["vigencias"]=vigencias
+    console.log(global_escuela)
 
 
     //console.log(GLOBAL.state.escuela)
